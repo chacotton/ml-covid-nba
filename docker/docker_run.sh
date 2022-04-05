4443860b1b89ace1c31b5f8e0bca8a8f2aca2284
Oracle Public Cloud User <opc@chase-dev.sub03141409550.modeldb.oraclevcn.com>
2022-04-05 17:38:38 +0000

    added automated build support

--- docker/docker_run.sh
+++ docker/docker_run.sh
@@ -1,2 +1,8 @@
-docker build -t helloapp:v1 .
-docker run -v $(pwd):/docker helloapp:v1
+#!/bin/bash
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  sudo systemctl start docker
+  sudo docker build -t helloapp:v1 .
+  sudo docker run -v $(pwd):/docker helloapp:v1
+  sudo systemctl stop docker
+fi
--- docker/hello_world.py
+++ docker/hello_world.py
@@ -1,6 +1,13 @@
 import time
+import requests
+from sqlalchemy import create_engine
+import pandas as pd
+import sys
 
 if __name__ == '__main__':
+    system = sys.platform
+    engine = create_engine(f'mysql+pymysql://admin:NBACovid19!@10.0.0.150/test_db')
+    engine.connect()
     with open('./hello_world.txt', 'w') as f:
         f.write('Hello World\n')
         f.write(f'Time: {time.time()}')
