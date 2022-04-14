2877ee915b60cb46e7d668546dec81f5329f06bc
Chhun Hong <56245196+chhunhorn@users.noreply.github.com>
2022-04-13 21:29:34 -0500

    Unit tests!

--- risk_reduction/chhun/write_dataframe.py
+++ risk_reduction/chhun/write_dataframe.py
@@ -16,18 +16,18 @@ def PandastoSQL(data, tableName):
     Method to covert Pandas Dataframe to SQL
     *Replace the table if table already exists
     """
-    data.to_sql(tableName, engine, if_exists='replace', index=False)
+    data.to_sql(tableName, engine, if_exists='append', index=False)
 
 dataset = chance_of_victory.WinDataset(optimize=True).get_dataset()
 tableName = 'pytabchhun'
 
 PandastoSQL(dataset, tableName)
 
-print(dataset.head(10))
+
 
 
 
 
 orders = """select * from pytabchhun"""
 df_orders = pd.read_sql(orders, engine)
-print(df_orders)
+
