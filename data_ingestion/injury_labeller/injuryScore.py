afea50ed8cfc211690ca16037fc7c728ed8e4478
Chase Cotton <chasepcotton@utexas.edu>
2022-08-22 13:45:49 -0500

    the root path makes no sense

--- data_ingestion/injury_labeller/injuryScore.py
+++ data_ingestion/injury_labeller/injuryScore.py
@@ -138,8 +138,8 @@ class InjuryScore:
         :param year: season year -> ex: represent 2016-2017 season as 2017
         :return: df for season played
         """
-        name_to_ref = pd.read_csv(os.environ.get('MLNBA_ROOT', '~/') +
-                                  "ml-covid-nba/data/injury_labeller/player_to_bballref.csv")
+        name_to_ref = pd.read_csv(os.environ.get('MLNBA_ROOT', '~/work/ml-covid-nba') +
+                                  "/ml-covid-nba/data/injury_labeller/player_to_bballref.csv")
         name_to_ref = name_to_ref.loc[name_to_ref['BBRefName'] == name]
         link = name_to_ref['BBRefLink'].values[0]
         link = link.replace('.html', '')
